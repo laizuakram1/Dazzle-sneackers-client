c332368bab8cbc4460f63e5b3335ec616ea8428d
laizu akram <laizuakram98@gmail.com>
2023-03-22 23:51:02 +0600

    get card error and display error

--- src/Components/Shop/Cart/Payment/CheckOutForm.js
+++ src/Components/Shop/Cart/Payment/CheckOutForm.js
@@ -1,46 +1,60 @@
-import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import React from 'react';
-
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import React, { useState } from "react";
 
 const CheckOutForm = () => {
-    const stripe = useStripe();
-    const elements = useElements();
+  const stripe = useStripe();
+  const elements = useElements();
+  const [cardError, setCardError] = useState('')
 
-    const handleSubmit =(event) =>{
-        event.preventDefault();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-        if(!stripe || !elements){
-            return;
-        }
-
-        const card = elements.getElement(CardElement);
-        if (card == null) {
-            return;
-          }
+    if (!stripe || !elements) {
+      return;
     }
-    return (
-        <form onSubmit={handleSubmit}>
-            <CardElement
-                options={{
-                    style: {
-                        base: {
-                            fontSize: '16px',
-                            color: '#424770',
-                            '::placeholder': {
-                                color: '#aab7c4',
-                            },
-                        },
-                        invalid: {
-                            color: '#9e2146',
-                        },
-                    },
-                }}
-            />
-            <button type="submit" disabled={!stripe}>
-                Pay
-            </button>
-        </form>
-    );
+
+    const card = elements.getElement(CardElement);
+    if (card == null) {
+      return;
+    }
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+    if(error){
+        setCardError(error.message);
+    }else{
+        setCardError('')
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <CardElement
+        options={{
+          style: {
+            base: {
+              fontSize: "16px",
+              color: "#424770",
+              "::placeholder": {
+                color: "#aab7c4",
+              },
+            },
+            invalid: {
+              color: "#9e2146",
+            },
+          },
+        }}
+      />
+      <p className="text-red-500">{cardError}</p>
+      <button
+        className="btn btn-sm btn-success mt-4"
+        type="submit"
+        disabled={!stripe}
+      >
+        Pay
+      </button>
+    </form>
+  );
 };
 
 export default CheckOutForm;
